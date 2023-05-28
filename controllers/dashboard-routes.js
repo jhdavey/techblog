a86094adfe91f06b63dd63e8f5c7fc634788ccda
jhdavey <jhdavey5182@gmail.com>
2023-05-28 17:43:17 -0400

    Fixed dashboard bug - works now

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,87 +1,80 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
-// Get all posts
-router.get('/', async (req, res) => {
-    try {
-        const dbPostData = await Post.findAll({ 
-            attributes: ['id', 'title', 'content', 'created_at'],           
-            include: [
-                {
-                    model: Comment,
-                    attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username'],
-                    },
-                },
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-            ],
-            order: [['created_at', 'DESC']],
-        })
-        // Serialize data retrieved
+// User's all posts ('/dashboard')
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+      attributes: ['id', 'title', 'content', 'created_at'],
+      order: [['created_at', 'DESC']],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    })
+      .then((dbPostData) => {
         const posts = dbPostData.map((post) => post.get({ plain: true }));
-
-        res.render('homepage', 
-            { posts, 
-            loggedIn: req.session.loggedIn, 
-            username: req.session.username,
-            userId: req.session.userId });
-    } catch (err) {
+        res.render('dashboard', { posts, loggedIn: true, username: req.session.username,});       
+      })
+      .catch((err) => {
+        console.log(err);
         res.status(500).json(err);
-    }
+    });
 });
 
-// Get single post ('/post/:id')
-router.get('/post/:id', async (req, res) => {
-    try{
-        const dbPostData = await Post.findOne({
-            where: {id: req.params.id},
-            attributes: ['id', 'content', 'title', 'created_at'],
-            include: [
-                {
-                    model: Comment,
-                    attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
-                    include: {
-                      model: User,
-                      attributes: ['username'],
-                    },
-                  },
-                  {
-                    model: User,
-                    attributes: ['username'],
-                  },
-            ],
-        });
-        if (dbPostData) {
-            const post = dbPostData.get({ plain: true });
-
-            res.render('single-post', { post, loggedIn: req.session.loggedIn, username: req.session.username, })  
-        } else {
-            res.status(404).json({ message: "This id has no post."});
-            return;
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }   
-});
-
-// Login
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
+// Get one post to edit ('dashboard/edit/:id')
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'title', 'content', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment', 'postId', 'userId', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'This id has no post.' });
         return;
-    }
-    res.render('login');
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render('edit-post', { post, loggedIn: true, username: req.session.username });         
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-// Signup
-router.get('/signup', async (req, res) => {
-    res.render('signup');
-})
+//  Get new post ('/dashboard/new)
+router.get('/new', withAuth, (req, res) => {
+    res.render('new-post', { username: req.session.username });
+});
 
 module.exports = router; 
